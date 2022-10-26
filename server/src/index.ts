fc0520a7581d5e3b5e183610db7fc8283a115835
Tim Reynolds <tim@timothyreynolds.co.uk>
2022-10-26 18:13:26 +0100

    chore: fix status code in active connection update code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-console.log('Starting server...');
+console.log("Starting server...");
 const users: Record<string, any> = {};
 
 const usersOnlineStatus = () =>
@@ -14,16 +14,16 @@ const usersOnlineStatus = () =>
   }));
 
 // [{ username: 'name', message: 'thing'}]
-const messages = [{ username: 'bot', message: 'Welcome to the chat!' }];
+const messages = [{ username: "bot", message: "Welcome to the chat!" }];
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_, res) => {
-  res.send('Hello World!');
+app.get("/", (_, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/message', (req, res) => {
+app.post("/message", (req, res) => {
   messages.push(req.body);
 
   res.sendStatus(200);
@@ -31,14 +31,14 @@ app.post('/message', (req, res) => {
   const usersWithOnline = usersOnlineStatus();
   Object.values(users).forEach((res: any) => {
     if (res) {
-      res
-        .sendStatus(200)
+      res.res
+        .status(200)
         .json({ messages: [req.body], users: usersWithOnline });
     }
   });
 });
 
-app.get('/poll', (req, res) => {
+app.get("/poll", (req, res) => {
   const { username, from = 0 } = (req.query as unknown) as {
     username: string;
     from: number;
@@ -50,7 +50,7 @@ app.get('/poll', (req, res) => {
     }
     users[username] = res;
 
-    req.on('close', function() {
+    req.on("close", function() {
       users[username] = null;
     });
   }
